92ba960c833d7cd03af75cc93e8dc780dede28c6
Jonas Häusler <haeusler@airmail.cc>
2020-02-26 14:58:49 +0100

    Add eisvogel template

--- handout.sh
+++ handout.sh
@@ -1,1 +1,1 @@
-pandoc handout.yaml handout.md --filter pandoc-crossref --filter pandoc-citeproc --data-dir ~/.pandoc --from markdown --template eisvogel131 -o handout.pdf
+pandoc handout.yaml handout.md --filter pandoc-crossref --filter pandoc-citeproc --from markdown --template eisvogel.latex -o handout.pdf
--- slides.sh
+++ slides.sh
@@ -1,1 +1,1 @@
-pandoc slides.md --top-level-division=chapter --slide-level=4 --data-dir ~/.pandoc --from markdown --to beamer --template eisvogel131.latex -i -o slides.pdf
+pandoc slides.md --top-level-division=chapter --slide-level=4 --from markdown --to beamer --template eisvogel.latex -i -o slides.pdf
